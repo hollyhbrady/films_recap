9b744c1ff2a64ccda95fc28041242d489db37e63
Colin Farquhar <colin.farquhar@codeclan.com>
2021-04-02 10:46:32 +0100

    initial commit

---
+++ films.py
@@ -0,0 +1,34 @@
+films = [
+    {
+        "title": "Alien",
+        "genre": "sci-fi",
+        "rating": 18,
+        "star": "Sigourney Weaver",
+        "costars": [
+            "John Hurt", 
+            "Tom Skerrit"
+            ]
+    },
+    {
+        "title": "Gladiator",
+        "genre": "drama",
+        "rating": 15,
+        "star": "Russell Crowe",
+        "costars": [
+            "Joaquin Phoenix", 
+            "Connie Nielsen", 
+            "Oliver Reed"
+            ]
+    },
+    {
+        "title": "Joker",
+        "genre": "drama",
+        "rating": 15,
+        "star": "Joaquin Phoenix",
+        "costars": [
+            "Robert De Niro"
+        ]
+    }
+]
+
+print(films)
